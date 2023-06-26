a265f1c71b1e9774ac2b1c1336c0d120045f06b4
Christopher Delgado <chrisdelgado221@gmail.com>
2023-06-25 18:05:46 -0700

    initial commit

---
+++ build/CMakeFiles/CLox.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/cygdrive/c/Users/chris/OneDrive/desktop/CLox/src/chunk.c" "/cygdrive/c/Users/chris/OneDrive/desktop/CLox/build/CMakeFiles/CLox.dir/src/chunk.c.o"
+  "/cygdrive/c/Users/chris/OneDrive/desktop/CLox/src/debug.c" "/cygdrive/c/Users/chris/OneDrive/desktop/CLox/build/CMakeFiles/CLox.dir/src/debug.c.o"
+  "/cygdrive/c/Users/chris/OneDrive/desktop/CLox/src/main.c" "/cygdrive/c/Users/chris/OneDrive/desktop/CLox/build/CMakeFiles/CLox.dir/src/main.c.o"
+  "/cygdrive/c/Users/chris/OneDrive/desktop/CLox/src/memory.c" "/cygdrive/c/Users/chris/OneDrive/desktop/CLox/build/CMakeFiles/CLox.dir/src/memory.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/CLox.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CLox.dir/src/chunk.c.o"
+  "CMakeFiles/CLox.dir/src/debug.c.o"
+  "CMakeFiles/CLox.dir/src/main.c.o"
+  "CMakeFiles/CLox.dir/src/memory.c.o"
+  "CLox.pdb"
+  "CLox.exe"
+  "libCLox.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CLox.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
